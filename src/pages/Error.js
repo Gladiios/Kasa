1a429af3b9b7a8aa7b839f7f3bc50b82b11f26bf
erwan <erwan-14730@outlook.fr>
2023-10-05 07:35:38 +0200

    responsive page error

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -6,13 +6,15 @@ import { NavLink } from "react-router-dom";
 const Error = () => {
   return (
     <div className="main-container">
-      <Header />
-      <div className="error-container">
-        <h1>404</h1>
-        <span>Oups! La page que vous demandez n'existe pas.</span>
-        <NavLink to="/">
-          <p id="home">Retourner sur la page d'accueil</p>
-        </NavLink>
+      <div className="second-container">
+        <Header />
+        <div className="error-container">
+          <h1>404</h1>
+          <span>Oups! La page que vous demandez n'existe pas.</span>
+          <NavLink to="/">
+            <p id="home">Retourner sur la page d'accueil</p>
+          </NavLink>
+        </div>
       </div>
       <Footer />
     </div>
